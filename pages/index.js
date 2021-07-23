7e7bde58b36ecc6307d0c25a41d3a1b24e5daf39
Rayan Kazi <rayan.quack.you@gmail.com>
2021-07-23 11:59:37 -0700

    finished the rest of the google front page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import Avatar from "@components/Avatar";
+import { ViewGridIcon, MicrophoneIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <Head>
         <title>Google Clone</title>
         <link rel="icon" href="/favicon.png" />
@@ -18,23 +21,34 @@ export default function Home() {
         <div className="flex space-x-4 items-center">
           <p className="link">Gmail</p>
           <p className="link">Images</p>
-
-          <svg
-            id="view-grid-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-          </svg>
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
 
           <Avatar url="https://lh3.googleusercontent.com/ogw/ADea4I6ue6ul3ozCcUYNy63qPsdyJ5zRRK5GenxA4mELWg=s192-c-mo" />
         </div>
       </header>
 
-      {/* Body */}
-      {/* Footer */}
+      <form className="flex flex-col items-center flex-grow mt-32 w-4/5">
+        <img
+          loading="lazy"
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          width={300}
+          className="object-contain"
+          height={300}
+        />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border-2 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="focus:outline-none flex-grow" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="button">Google Search</button>
+
+          <button className="button">I'm Feeling Lucky</button>
+        </div>
+      </form>
+
+      <Footer />
     </div>
   );
 }
